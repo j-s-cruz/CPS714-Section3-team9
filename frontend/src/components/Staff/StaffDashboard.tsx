6842dab54db84e31984363d9ef5a65c7330b7998
Reyhan Emik <emikreyhan@gmail.com>
2025-11-14 15:47:17 -0500

    add view admins and class management feautures (team 7)

--- frontend/src/components/Staff/StaffDashboard.tsx
+++ frontend/src/components/Staff/StaffDashboard.tsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react';
 import { admin_supabase } from './supabaseClient';
-import { Users, Calendar, TrendingUp, DollarSign, Plus, Bell, BarChart3 } from 'lucide-react';
-import DumbbellIcon from '../../assets/dumbbell.png';
-
+import { Users, Calendar, TrendingUp, Plus, Bell, BarChart3, UserIcon, SparkleIcon } from 'lucide-react';
+// import DumbbellIcon from '../../assets/dumbbell.png';
+import { FaDumbbell } from "react-icons/fa";
 
 
 export const StaffDashboard = () => {
@@ -12,6 +12,7 @@ export const StaffDashboard = () => {
     totalClasses: 0,
     todayBookings: 0,
   });
+  const [showViewAdmins, setshowViewAdmins] = useState(false); //hide all modals (only open on click)
   const [showAddClass, setShowAddClass] = useState(false);
   const [showAnnouncement, setShowAnnouncement] = useState(false);
 
@@ -42,12 +43,20 @@ export const StaffDashboard = () => {
   return (
     <div className="min-h-screen bg-gray-900 text-slate-500 space-y-6 p-6">
       <div className="flex items-center justify-between">
-        <h2 className="text-2xl font-bold text-slate-100 flex items-center gap-2">Staff Dashboard
-          <img src={DumbbellIcon} alt="Dashboard DumbbellIcon Icon" className='w-10 h-9 relative -top-0.5'/>
-        </h2>
+        <div className='flex items-center gap-2'>
+          <FaDumbbell className='w-7 h-7 text-yellow-500 relative -top-0.5'/>
+          <h2 className="text-2xl font-bold text-slate-100 flex items-center gap-2">Staff Dashboard</h2>
+        </div>
         <div className="flex gap-2">
           <button
-            onClick={() => setShowAddClass(true)}
+            onClick={() => setshowViewAdmins(true)}
+            className="px-4 py-2 bg-yellow-500 text-black rounded-lg font-semibold hover:bg-yellow-500 transition flex items-center gap-2"
+          >
+            <UserIcon className="w-4 h-4" />
+            View Admins
+          </button>
+          <button
+            onClick={() => setShowAddClass(true)} //update state
             className="px-4 py-2 bg-yellow-500 text-black rounded-lg font-semibold hover:bg-yellow-500 transition flex items-center gap-2"
           >
             <Plus className="w-4 h-4" />
@@ -64,7 +73,7 @@ export const StaffDashboard = () => {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
-        <div className="bg-gray-800 rounded-xl p-6 shadow-md border border-white">
+        <div className="bg-gray-800 rounded-xl p-6 shadow-md border border-yellow-500">
           <div className="flex items-center justify-between mb-2">
             <span className="text-white">Total Members</span>
             <Users className="w-5 h-5 text-orange" />
@@ -72,7 +81,7 @@ export const StaffDashboard = () => {
           <p className="text-3xl font-bold text-white">{stats.totalMembers}</p>
         </div>
 
-        <div className="bg-gray-800 rounded-xl p-6 shadow-md border border-white">
+        <div className="bg-gray-800 rounded-xl p-6 shadow-md border border-yellow-500">
           <div className="flex items-center justify-between mb-2">
             <span className="text-white">Active Subscriptions</span>
             <TrendingUp className="w-5 h-5 text-green-600" />
@@ -80,7 +89,7 @@ export const StaffDashboard = () => {
           <p className="text-3xl font-bold text-white">{stats.activeMembers}</p>
         </div>
 
-        <div className="bg-gray-800 rounded-xl p-6 shadow-md border border-white">
+        <div className="bg-gray-800 rounded-xl p-6 shadow-md border border-yellow-500">
           <div className="flex items-center justify-between mb-2">
             <span className="text-white">Total Classes</span>
             <Calendar className="w-5 h-5 text-blue-500" />
@@ -88,7 +97,7 @@ export const StaffDashboard = () => {
           <p className="text-3xl font-bold text-white">{stats.totalClasses}</p>
         </div>
 
-        <div className="bg-gray-800 rounded-xl p-6 shadow-md border border-white">
+        <div className="bg-gray-800 rounded-xl p-6 shadow-md border border-yellow-500">
           <div className="flex items-center justify-between mb-2">
             <span className="text-white">Today's Bookings</span>
             <BarChart3 className="w-5 h-5 text-amber-600" />
@@ -104,6 +113,7 @@ export const StaffDashboard = () => {
         <MemberManagement />
         <ClassManagement />
       </div>
+      {showViewAdmins && <ViewAdmins onClose={() => setshowViewAdmins(false)}/>} 
       {showAddClass && <AddClassModal onClose={() => setShowAddClass(false)} />}
       {showAnnouncement && <AnnouncementModal onClose={() => setShowAnnouncement(false)} />}
     </div>
@@ -126,7 +136,8 @@ const Report = () => {
         style={{
         width: "100%",
         height: "300px", // reserve height for charts
-        border: "2px dashed #ccc",
+        border: "2px solid #ca8a04",
+        borderRadius: "12px",
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
@@ -182,7 +193,7 @@ const MemberManagement = () => {
   };
 
   return (
-    <div className="bg-gray-800 rounded-xl p-6 shadow-sm border border-white self-start">
+    <div className="bg-gray-800 rounded-xl p-6 shadow-sm border border-yellow-500 self-start">
       <button
         onClick={() => setOpen(!open)}
         className="w-full text-left text-white font-bold text-lg mb-4 flex justify-between items-center"
@@ -216,6 +227,8 @@ const MemberManagement = () => {
 const ClassManagement = () => {
   const [classes, setClasses] = useState<any[]>([]);
   const [openClass, setOpenClass] = useState(false);
+  const [selectedClass, setSelectedClass] = useState<any | null>(null);
+  const [showClassModal, setShowClassModal] = useState(false);
 
   useEffect(() => {
     fetchAllClasses();
@@ -246,8 +259,9 @@ const ClassManagement = () => {
   }
   };
 
+
   return (
-     <div className="bg-gray-800 rounded-xl p-6 shadow-sm border border-white self-start">
+     <div className="bg-gray-800 rounded-xl p-6 shadow-sm border border-yellow-500 self-start">
         <button
           onClick={() => setOpenClass(!openClass)}
           className="w-full text-left text-white font-bold text-lg mb-4 flex justify-between items-center"
@@ -255,27 +269,117 @@ const ClassManagement = () => {
           Class Management
           <span className="text-slate-300 text-l">{openClass ? '-' : '+'}</span> 
         </button>
-        {openClass &&
+       
+        {openClass && (
           <div className="space-y-3 max-h-[600px] overflow-y-auto">
             {classes.map((cls, index) => (
-              <div key={cls.id} className="flex items-center justify-between p-3 bg-slate-50 rounded-lg">
+              <button
+                key={cls.id}
+                onClick={() => {
+                  setSelectedClass(cls);
+                  setShowClassModal(true);
+                }}
+                className="w-full text-left p-3 bg-slate-50 rounded-lg flex items-center justify-between hover:bg-slate-100 transition"
+              >
                 <div className="flex items-center gap-3">
                   <div className="w-8 h-8 rounded-full bg-slate-800 text-white flex items-center justify-center font-bold">
                     {index + 1}
                   </div>
+
                   <div>
-                    <p className="font-semibold text-yellow-600">{cls.class_name}</p>
-                    <p className="text-sm text-slate-600">{cls.instructor_name}</p>
-                    <p className="text-xs text-slate-600">{cls.day} {cls.time}</p>
+                    <p className="inline font-semibold text-yellow-600">{cls.class_name}: </p>
+                    <p className="inline text-xs text-slate-600">{cls.class_type}</p>
+                    <p className="text-sm text-slate-600">Instructor: {cls.instructor_name}</p>
+                    <p className="text-sm text-slate-600">Date: {cls.day}</p>
+                    <p className="text-sm text-slate-600">Time: {cls.time.slice(0, -3)}</p> 
                   </div>
                 </div>
-                <span className="text-sm font-semibold text-slate-700">Bookings: {cls.total_bookings} Capacity: {cls.capacity} </span>
-              </div>
+
+                <span className="text-sm font-semibold text-slate-700">
+                  Capacity: {cls.capacity} <br />Bookings: {cls.total_bookings}
+                </span>
+              </button>
             ))}
           </div>
-        }
+        )}
+
+      {showClassModal && (
+        <ClassDetailsModal
+          cls={selectedClass}
+          onClose={() => { setShowClassModal(false); setSelectedClass(null);}}
+        />
+      )}
+     </div>
+    );
+  };
+
+  
+const ClassDetailsModal = ({ cls, onClose }: { cls: any, onClose: () => void}) => {
+  const label = "font-semibold text-yellow-600";
+  
+  const deleteClass = async (classId : string) => {
+    try{
+
+      if(!confirm("Are you sure you want to delete this class?")) return;
+      
+      const result = await admin_supabase
+      .from('class')
+      .delete()
+      .eq('id', classId);
+      
+      const error = result.error;
+      
+      if (error) {
+        throw error;
+      }else{
+        alert("Class was succesfully deleted");
+        onClose();
+      }
+    }catch (err: any){
+      console.error("An Error occured", err.message)
+      alert("Error deleting class");
+    }
+  };
+  
+  if (!cls) return null
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+      <div className="bg-gray-800 rounded-xl p-6 w-full max-w-md border border-yellow-500">
+        <div className="flex justify-between items-start mb-4">
+          <h3 className="text-lg font-bold uppercase tracking-wide text-white">{cls.class_name}</h3>
+          <button onClick={onClose} className="text-white text-xl">x</button>
+        </div>
+        
+          <div className="space-y-1 text-sm text-slate-200 leading-6">
+            <span className={label}>Class Type: </span>{cls.class_type}<br />
+            <span className={label}>Instructor: </span>{cls.instructor_fname}<br />
+            <span className={label}>Date: </span>{cls.day}<br />
+            <span className={label}>Time: </span>{cls.time.slice(0, -3)}<br />
+            <span className={label}>Class Capacity: </span>{cls.capacity}<br />
+            <span className={label}>Total Bookings: </span>{cls.total_bookings}
+          </div>
+
+        
+        <div className="mt-5">
+          <button
+            onClick={ () => deleteClass(cls.id)}
+            className='w-full px-4 py-2 bg-red-600 text-black rounded-lg font-semibold'
+          >
+            Delete Class
+          </button>
+          
+          <button
+            onClick={onClose}
+            className='w-full mt-3 px-4 py-2 bg-yellow-600 text-black rounded-lg font-semibold'
+          >
+            Close Window
+          </button>
+        </div>
+
+        </div>
     </div>
   );
+
 };
 
 //COMPONENT: ADD CLASS FEAUTURE
@@ -287,14 +391,15 @@ const AddClassModal = ({ onClose }: { onClose: () => void }) => {
     capacity: '',
     date: '',
     time: '',
+    class_type: '',
   });
- 
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const { error } = await admin_supabase.from('Class').insert({
+      const { error } = await admin_supabase.from('class').insert({
         class_name: formData.classId,
+        class_type: formData.class_type.toUpperCase(),
         instructor_fname: formData.first_name, 
         instructor_lname: formData.last_name,
         capacity: formData.capacity,
@@ -332,6 +437,18 @@ const AddClassModal = ({ onClose }: { onClose: () => void }) => {
               className="w-full px-4 py-2 border border-slate-300 rounded-lg focus:ring-2 focus:ring-blue-500"
               required
               placeholder="ex: Boxing and Kickboxing"
+            />
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium text-yellow-600 mb-2">Class Type</label>
+            <input
+              type="text"
+              value={formData.class_type}
+              onChange={(e) => setFormData({ ...formData, class_type: e.target.value })}
+              className="w-full px-4 py-2 border border-slate-300 rounded-lg focus:ring-2 focus:ring-blue-500 italic"
+              required
+              placeholder="ex: BASIC, PREMIUM, VIP"
             />
           </div>
 
@@ -415,6 +532,51 @@ const AddClassModal = ({ onClose }: { onClose: () => void }) => {
   );
 };
 
+//COMPONENT: VIEW ADMINS 
+const ViewAdmins = ({onClose}: {onClose: () => void}) => {
+  const admins = [
+    {id: 1,full_name: 'Reyhan Emik'},
+    {id: 2,full_name: 'Rana Hamood'},
+    {id: 3,full_name: 'Nafia Rahman'},
+    {id: 4,full_name: 'Alaa Yafaoui'},
+    {id: 5,full_name: 'Jasmine Jawanda'},
+  ];
+
+  return(
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+      <div className="bg-slate-200 rounded-xl border-8 border-slate-700 p-6 max-w-md w-full m-4"> 
+        <h3 className="text-xl font-bold text-slate-800 mb-4">FitHub Administrators</h3>
+        <div className="space-y-2">
+
+          {admins.map((admin) => (
+            <div
+              key={admin.id}
+              className="p-3 bg-slate-200 rounded-lg text-slate-900"
+            >
+            <div className="flex items-center gap-2 ">
+                <SparkleIcon className="w-4 h-4" />
+                <span>{admin.full_name}</span>
+            </div>
+            </div>
+          ))}
+
+        </div>
+        <button
+          onClick={onClose}
+          className="mt-4 w-full px-4 py-2 bg-yellow-600 text-black rounded-lg font-semibold"
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  );
+
+};
+
+
+
+
+//REMOVE COMPONENT LATER NOT NEEDED FOR NOW
 const AnnouncementModal = ({ onClose }: { onClose: () => void }) => {
   const [formData, setFormData] = useState({
     title: '',
